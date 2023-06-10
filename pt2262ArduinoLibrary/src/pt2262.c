3c612d1ca5ba0a3ac0571e373d5db1c157cdd9bc
haihif <haihif2002@gmail.com>
2023-06-10 21:57:51 +0700

    commit change

---
+++ pt2262ArduinoLibrary/src/pt2262.c
@@ -0,0 +1,9 @@
+#include "pt2262.h"
+
+void function1() {
+  // Thực hiện các thao tác của function1
+}
+
+int function2(int parameter) {
+  // Thực hiện các thao tác của function2 và trả về kết quả
+}
---
+++ pt2262ArduinoLibrary/src/pt2262.h
@@ -0,0 +1,19 @@
+#ifndef PT2262_H
+#define PT2262_H
+
+#include <Arduino.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+void function1();
+int function2(int parameter);
+// Khai báo các hàm và biến khác tại đây
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
